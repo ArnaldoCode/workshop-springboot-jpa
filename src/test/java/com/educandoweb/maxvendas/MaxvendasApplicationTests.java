0b2db601b10f8f13244f4825af975106fff89bde
ArnaldoCode <popoarnaldo@hotmail.com>
2026-03-01 19:21:47 -0300

    Project created

---
+++ src/test/java/com/educandoweb/maxvendas/MaxvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.maxvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
