be1af3ccefea36f962416d39f915392eccf26cc3
ArnaldoCode <popoarnaldo@hotmail.com>
2026-04-05 22:42:06 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/maxvendas/resources/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/maxvendas/resources/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.educandoweb.maxvendas.resources.exceptions.StandardError;
+import com.educandoweb.maxvendas.services.exceptions.DatabaseException;
 import com.educandoweb.maxvendas.services.exceptions.ResourcedNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resouceNotFound(ResourcedNotFoundException e, HttpServletRequest request){
 		String error = "Resourced Not Found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database (DatabaseException e, HttpServletRequest request){
+		String error = "Resourced Not Found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
